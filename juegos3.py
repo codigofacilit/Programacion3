b24bffca7caf9f6d88a492d1fb401a59362baca9
codigofacilit <saravimanueljuanpablo@gmail.com>
2023-08-22 18:26:17 -0400

    version 2023-08-22_02

---
+++ juegos3.py
@@ -0,0 +1,17 @@
+import ahorcado
+import adivinanza
+
+print('================================')
+print('Elija su Juego')
+print('================================')
+
+print("(1) Ahorcado (2) Adivinanza")
+
+juego = int(input("¿Cuál juego? "))
+
+if (juego == 1):
+    print("Jugando ahorcado")
+    horca.jugar_ahorcado()
+elif (juego == 2):
+    print("Jugando adivinanza")
+    adivinanza.jugar_adivinanza()
