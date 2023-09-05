07e1f78017ad6f5f34d07fae27052e6955659e67
codigofacilit <saravimanueljuanpablo@gmail.com>
2023-09-05 17:50:38 -0400

    version 2023-09-05_03

--- Ahorcados2.py
+++ Ahorcados2.py
@@ -4,20 +4,28 @@ def jugar():
     print('========================================')
 
     palabra_secreta = 'mandarina'
+    letras_acertadas =['','','','','','','','','_']
     
     ahorcado = False
     acerto = False
     
+    print(letras_acertadas)
     while(not ahorcado and not acerto):
         entrada= input('Ingrese una letra... ')
+        entrada = entrada.strip()
+        entrada = entrada.lower()
+        indice = 0
         for letra in palabra_secreta:
             if(entrada==letra):
-                print(entrada)
-        
+                letras_acertadas[indice] = letra
+                #print('Se encontró la letra {} en la posición {}'.format(letra, indice))
+                
+            indice = indice + 1
+        print(letras_acertadas)
         print('jugando... ')
         
         
     print('FIN DEL JUEGO')
     
-if(__name__=="__main__"):
+if(__name__ == "__main__"):
     jugar()
